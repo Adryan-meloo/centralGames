359502da28e06a5f0d7534492f8b91bc2df6019f
Adryan <adryan.george.ti@gmail.com>
2024-05-28 11:14:22 -0300

    alterações no mobile

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function menuShow(){
-    let menuMobile = document.querySelector('.mobile-menu');
-    if ( menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./img/menu-mobile.png";
-    }else{
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "./img/x.png";
-
-    }
-}
+addEventListener('scroll',function(){
+    let cabecalho = document.querySelector('.navigation');
+    cabecalho.classList.toggle('scrollando',window.scrollY > 0);
+});
