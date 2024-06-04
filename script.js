b02cab2ce891459b6efd8904346da02e96204f0a
Adryan <adryangeorgen@gmail.com>
2024-06-03 21:38:53 -0300

    alterações nos cards

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ addEventListener('scroll',function(){
     let cabecalho = document.querySelector('.navigation');
     cabecalho.classList.toggle('scrollando',window.scrollY > 0);
 });
+
+let iconMobile = document.querySelector('.mobile-menu-icon');
+let menuMobile = document.querySelector('.mobile-menu');
+
+iconMobile.addEventListener('click',function(){
+    if(menuMobile.style.display == 'none'){
+        menuMobile.style.display = 'block';
+    }else{
+        menuMobile.style.display = 'none';
+    }
+});
+
+let gta = document.querySelector('#goGta');
+let cs = document.querySelector('#goCs');
+let lol = document.querySelector('#goLol');
+
+
+
+gta.addEventListener('click',function(){
+    
+    window.location.href = 'https://www.rockstargames.com/br/games/sanandreas';
+});
+
+cs.addEventListener('click',function(){
+    window.location.href = 'https://store.steampowered.com/app/730/CounterStrike_2/?l=portuguese';
+});
+
+lol.addEventListener('click',function(){
+    window.location.href = 'https://signup.leagueoflegends.com/pt-br/signup/redownload';
+});
